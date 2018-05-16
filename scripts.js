6ab764ea4e25d608e21af2accc376b7f57b46000
Aleksey Vlasov <Aleksey.Vlasov@gmail.com>
2018-05-16 13:11:28 -0700

    Add click transition as proof of concept

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 window.onload = function() {
   document.getElementById("topmenubutton").addEventListener('click', openNav);
   document.getElementById("close-menu").addEventListener('click', closeNav);
+  document.getElementById("page1").addEventListener('click', scroll2);
+  document.getElementById("page2").addEventListener('click', scroll1);
 }
 
 function openNav() {
@@ -11,4 +13,14 @@ function openNav() {
 function closeNav() {
   document.getElementById("nav").style.height = "0%";
   document.body.classList.remove("open");
+}
+
+function scroll2() {
+  document.getElementById("page1").style.display = "none";
+  document.getElementById("page2").style.display = "block";
+}
+
+function scroll1() {
+  document.getElementById("page1").style.display = "block";
+  document.getElementById("page2").style.display = "none";
 }
