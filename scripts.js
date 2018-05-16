56f3ef7375bad96141bb88454c827bde1d5092d4
Aleksey Vlasov <Aleksey.Vlasov@gmail.com>
2018-05-16 15:02:44 -0700

    Got Scrolling animation working

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 window.onload = function() {
   document.getElementById("topmenubutton").addEventListener('click', openNav);
   document.getElementById("close-menu").addEventListener('click', closeNav);
-  document.getElementById("page1").addEventListener('click', scroll2);
-  document.getElementById("page2").addEventListener('click', scroll1);
 }
 
 function openNav() {
@@ -13,14 +11,4 @@ function openNav() {
 function closeNav() {
   document.getElementById("nav").style.height = "0%";
   document.body.classList.remove("open");
-}
-
-function scroll2() {
-  document.getElementById("page1").style.display = "none";
-  document.getElementById("page2").style.display = "block";
-}
-
-function scroll1() {
-  document.getElementById("page1").style.display = "block";
-  document.getElementById("page2").style.display = "none";
 }
